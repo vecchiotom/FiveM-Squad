4b3b4f78ce8c75c0063fd5466f9bf978933ab0f9
IceHax <vecchiotom1@gmail.com>
2018-09-08 14:26:47 +0200

    add version.json and updater

--- server/main.lua
+++ server/main.lua
@@ -3,3 +3,25 @@ local players = {}
 AddEventHandler("playerConnecting", function()
     table.insert(players, {id=source,team="",squad=0,kills=0,deaths=0})
 end)
+
+local verFile = LoadResourceFile(GetCurrentResourceName(), "version.json")
+local curVersion = json.decode(verFile).version
+Citizen.CreateThread( function()
+	local updatePath = "/vecchiotom/FiveM-Squad"
+	local resourceName = "SpotiFive ("..GetCurrentResourceName()..")"
+	PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version.json", function(err, response, headers)
+		local data = json.decode(response)
+
+
+		if curVersion ~= data.version and tonumber(curVersion) < tonumber(data.version) then
+			print("\n--------------------------------------------------------------------------")
+			print("\n"..resourceName.." is outdated.\nCurrent Version: "..data.version.."\nYour Version: "..curVersion.."\nPlease update it from https://github.com"..updatePath.."")
+			print("\nUpdate Changelog:\n"..data.changelog)
+			print("\n--------------------------------------------------------------------------")
+		elseif tonumber(curVersion) > tonumber(data.version) then
+			print("Your version of "..resourceName.." seems to be higher than the current version.")
+		else
+			print(resourceName.." is up to date!")
+		end
+	end, "GET", "", {version = 'this'})
+end)
